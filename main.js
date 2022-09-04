135d07d83d885381681e319d79faf2183cdbd586
Mateusz Melaniuk <mateusz.melaniuk98@gmail.com>
2022-09-04 16:16:10 +0200

    Add JS and Animations on Sign In/Up buttons

---
+++ main.js
@@ -0,0 +1,11 @@
+const signin_btn = document.querySelector("#signin-btn");
+const signup_btn = document.querySelector("#signup-btn");
+const container = document.querySelector(".container");
+
+signup_btn.addEventListener("click", () => {
+  container.classList.add("signup-mode");
+});
+
+signin_btn.addEventListener("click", () => {
+  container.classList.remove("signup-mode");
+});
